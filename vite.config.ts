e57d90fe378b8dadea89e7a3b4be6582947b325b
msrwilo <msrwilo@gmail.com>
2026-05-02 07:27:01 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  root: 'src/routes',
-  base: "/cer-mica-studio-pro/",   // 👈 nombre de tu repo
-  plugins: [react()],
-})
+
+export default defineConfig();
